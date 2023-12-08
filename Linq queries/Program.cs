a1971d095d08c821a8b80b11bf65306de13a4b98
USER <USER@DESKTOP-SNMHLDN>
2023-12-08 17:24:16 +0100

    Add project files.

---
+++ Linq queries/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+{
+    //ICollection 
+    //IList
+    //List
+    private static void Main(string[] args)
+    {
+        List<string> Data = new List<string>();
+        Data.Add("chalk");
+        Data.Add("tyrone");
+        Data.Add("clone");
+        Data.Add("reserve");
+        Data.Add("align");
+        Data.Add("disregard");
+    }
+    public static void CollectionDisplay (ICollection<string> mylist)
+    {
+        foreach(var item in mylist)
+        {
+            Console.WriteLine("the List elements are as follows:"+ item);
+        }
+    }
+    public static void IListDisplay(ICollection<string> mylist)
+    {
+        foreach (var item in mylist)
+        {
+            Console.WriteLine("the List elements are as follows:" + item);
+        }
+    }
+    public static void ListDisplay(ICollection<string> mylist)
+    {
+        foreach (var item in mylist)
+        {
+            Console.WriteLine("the List elements are as follows:" + item);
+        }
+    }
+}
